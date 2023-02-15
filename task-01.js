ea267c7e88b8cd6cc6ac3227562a865b64fb5df1
NataliaNevidoma <56003837+NataliaNevidoma@users.noreply.github.com>
2023-02-15 22:39:13 +0200

    add links for task 01

--- task-01.js
+++ task-01.js
@@ -1,1 +1,11 @@
+ //https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
