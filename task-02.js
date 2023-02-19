44df9166e5501436697f3a3287a50c8873faba30
NataliaNevidoma <56003837+NataliaNevidoma@users.noreply.github.com>
2023-02-19 15:27:34 +0200

    Create task-02.js

---
+++ task-02.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
