40a1164c207960fac08d8641820cc69d442e3621
NataliaNevidoma <56003837+NataliaNevidoma@users.noreply.github.com>
2023-02-19 15:33:06 +0200

    Update task-02.js

--- task-02.js
+++ task-02.js
@@ -1,10 +1,29 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
+function stringToArray(string) {
+  return string.split(" ");
+}
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  return dna.replaceAll("T", "U");
+}
 
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
+var min = function (list) {
+  return Math.min(...list);
+};
+
+var max = function (list) {
+  return Math.max(...list);
+};
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+function min(arr, toReturn) {
+  let minValue = Math.min(...arr);
+  return toReturn === "value" ? minValue : arr.indexOf(minValue);
+}
