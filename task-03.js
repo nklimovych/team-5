7a14549c207b3b400872e57ff8d0d5cd0606ace9
NataliaNevidoma <56003837+NataliaNevidoma@users.noreply.github.com>
2023-02-21 15:24:29 +0200

    Update task-03.js

--- task-03.js
+++ task-03.js
@@ -1,1 +1,31 @@
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}
+
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+function giveMeFive(obj) {
+  const arr = [];
+  for (var key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
+    }
+    if (obj[key].length === 5) {
+      arr.push(obj[key]);
+    }
+  }
+  return arr;
+}
